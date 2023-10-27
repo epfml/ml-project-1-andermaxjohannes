64bb0fec416b0e70dd3161d40063ca46d04bb139
ch-epfl-367983 <ch-epfl-367983@noto.epfl.ch>
2023-10-27 13:45:36 +0200

    Some fun cross valid stuff

--- implementations.py
+++ implementations.py
@@ -546,6 +546,83 @@ def k_fold_cross_validation_sets(y,x,K):
         
         yield y[indices_k], x[indices_k] # Yield returns the first set, and next time the function is called the code continues, so the for loop repeats and yields the next set
 
+def max_k_fold_cross_valid_sets(y,k_fold):
+    """build k indices for k-fold.
+
+    Args:
+        y:      shape=(N,)
+        k_fold: K in K-fold, i.e. the fold num
+        seed:   the random seed
+
+    Returns:
+        A 2D array of shape=(k_fold, N/k_fold) that indicates the data indices for each fold
+
+    >>> build_k_indices(np.array([1., 2., 3., 4.]), 2, 1)
+    array([[3, 2],
+           [0, 1]])
+    """
+    num_row = y.shape[0]
+    interval = int(num_row / k_fold)
+    #np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k * interval : (k + 1) * interval] for k in range(k_fold)]
+    #print(np.array(k_indices))
+    return np.array(k_indices)
+
+def max_cross_valid(y, x, k_indices, k_fold, lambda_, degree):
+    """return the loss of ridge regression for a fold corresponding to k_indices
+
+    Args:
+        y:          shape=(N,)
+        x:          shape=(N,)
+        k_indices:  2D array returned by build_k_indices()
+        k:          scalar, the k-th fold (N.B.: not to confused with k_fold which is the fold nums)
+        lambda_:    scalar, cf. ridge_regression()
+        degree:     scalar, cf. build_poly()
+
+    Returns:
+        train and test root mean square errors rmse = sqrt(2 mse)
+
+    >>> cross_validation(np.array([1.,2.,3.,4.]), np.array([6.,7.,8.,9.]), np.array([[3,2], [0,1]]), 1, 2, 3)
+    (0.019866645527597114, 0.33555914361295175)
+    """
+
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # get k'th subgroup in test, others in train: TODO
+    k_indices = max_k_fold_cross_valid_sets(y, k_fold
+                                           )
+   
+    
+    
+    w, loss = np.zeros((k_fold,tx.shape[1])), np.zeros(k_fold)
+    
+    for k in range(k_fold):
+        test_ind = k_indices[k]
+        train_ind = (k_indices[np.arange(len(k_indices))!=k]).flatten()
+
+        x_train = x[train_ind]
+        x_test = x[test_ind]
+
+        y_train = y[train_ind]
+        y_test = y[test_ind]
+        
+        w[k], loss[k] = regressionFunction(y_test, x_test, initial_w, max_iters, gamma)
+
+        print(f'Run {k+1} yielded a loss improvement from {lossFunction(y_k,tx_k,initial_w)} to {lossFunction(y_k,tx_k,w[k])}')
+    w_avg = np.sum(w,axis=0) / K
+    
+    print(f'''-----------------------------------------------------------------------------------------
+Averaging the parameters, the loss improves from {lossFunction(y,tx,initial_w)} to {lossFunction(y,tx,w_avg)}''')
+    return w_avg, lossFunction(y_k,tx_k,initial_w)
+    # ***************************************************
+    #raise NotImplementedError
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # form data with polynomial degree: TODO
+    
+    return loss_tr, loss_te
+
 def k_fold_cross_validation(y,tx,K,initial_w,max_iters,gamma, regressionFunction=logistic_regression, lossFunction=logistic_loss):
     ''' Performing regression on K separate subsets of the provided training set, and returning the average parameters
     Args:
